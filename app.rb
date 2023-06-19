acf8a1581d76dc067550144a49ef327361d321b6
reaganmunene <reagannmunene@gmail.com>
2023-06-19 15:05:24 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -1,23 +1,9 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
-
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
-
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
-
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
+# puts "hello world"
+# puts "Hello world!"
+puts "Hello World!"
+# puts "Hello world!"
+# print "Hello world!"
+print "Pass this test, please."
+# puts [1,2,3 ,4,5,6,7,8,9,10,11,12,13,14]
+p [1,2,3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
